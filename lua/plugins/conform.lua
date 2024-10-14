d860a3cf79f94cd3f9fc776c39381b836e100c91
Le Anh Quang <quangnb2000@gmail.com>
2024-10-14 15:05:20 +0700

    Add default conform.nvim formatters

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -71,4 +71,17 @@ return {
       if autoformat then return { timeout_ms = 500 } end
     end,
   },
+  config = function()
+    require("conform").setup {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- Conform will run multiple formatters sequentially
+        python = { "isort", "black" },
+        -- You can customize some of the format options for the filetype (:help conform.format)
+        rust = { "rustfmt", lsp_format = "fallback" },
+        -- Conform will run the first available formatter
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+    }
+  end,
 }
